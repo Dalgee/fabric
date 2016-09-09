ee236cd15f2765a0a316f2e7c592c1e193e8ec2e
kim seok yoon <seokyoon.kim@gmail.com>
2016-09-09 19:40:26 +0930

    under building

---
+++ config.py
@@ -0,0 +1,25 @@
+configs = {
+    "node01" : {
+        "ip": "192.168.0.106",
+        "password": "",
+        "roles": ["hadoop-namenode", "hadoop-datanode", "elasticsearch"]
+    },
+
+    "node02" : {
+        "ip": "192.168.0.106",
+        "password": "",
+        "roles": ["hadoop-datanode", "elasticsearch"]
+    },
+
+    "node03" : {
+        "ip": "192.168.0.106",
+        "password": "",
+        "roles": ["hadoop-datanode", "elasticsearch"]
+    },
+
+    "node04" : {
+        "ip": "192.168.0.106",
+        "password": "",
+        "roles": ["hadoop-datanode", "elasticsearch"]
+    }
+}
--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,17 @@
-from fabric.api import *
+from fabric.api import run
+from fabric.context_managers import env
+from config import configs
+from StringIO import StringIO
+
+
+for (node, value) in configs.items():
+     temp = "%s@%s:%s" % ('root', node, '22')
+
+     env.passwords[temp] = value['password']
+     env.hosts.append(node)
+
+
+def setup():
+    """ """
+    msg = run("apt-update")
+    print msg
